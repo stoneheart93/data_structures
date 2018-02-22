7489370cce8f15c659c3d169e24ca91756d301c8
stoneheart93 <sohinee93@gmail.com>
2018-02-22 14:08:37 +0530

    Update edit_distance.c

--- DP/edit_distance.c
+++ DP/edit_distance.c
@@ -18,8 +18,8 @@ int min(int a, int b, int c)
  
 int editDistance(char* str1, char* str2, int m, int n)
 {
-    int dp[m + 1][n + 1];
- 	int i, j;
+	int dp[m + 1][n + 1];
+	int i, j;
     for(i = 0; i <= m; i++)
     {
         for(j = 0; j <= n; j++)
@@ -27,7 +27,7 @@ int editDistance(char* str1, char* str2, int m, int n)
             if(i == 0)
                 dp[i][j] = j;  
 			else if(j == 0)
-                dp[i][j] = i; 
+                dp[i][j] = i;
             else if (str1[i - 1] == str2[j - 1])
                 dp[i][j] = dp[i - 1][j - 1];
  			else
