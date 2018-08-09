182763f6e7c20b50151991120fac4efbae7ea6b2
stoneheart93 <sohinee93@gmail.com>
2018-08-09 14:52:14 +0530

    sumproduct

---
+++ Array/sumProduct.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int sumProduct(int a[], int n)
+{
+	int i, res = 0;
+	int sum[n];
+	
+	sum[n - 1] = a[n - 1];
+	for(i = n - 2; i >= 0; i--)
+	{
+		sum[i] = sum[i + 1] + a[i];
+	}
+	
+	for(i = 0; i < n - 1; i++)
+	{
+		res += a[i] * sum[i + 1]; 
+	}
+	return res;
+}   
+ 
+int main() 
+{
+    int n, i;
+	int a[20];
+	scanf("%d", &n);
+	for(i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	printf("%d ", sumProduct(a, n));
+	return 0;
+}
